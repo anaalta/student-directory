223cff7fa874618fd274276b71c8c5e85b17c7fd
anaalta <suciuanamaria@yahoo.com>
2017-03-29 20:08:20 +0200

    8.5 extra info in the hash

---
+++ directory_8.5.rb
@@ -0,0 +1,51 @@
+def input_students
+  puts "Please enter the name of the student:"
+  puts "To finish, just hit return twice"
+#an empty array
+  students = []
+#get the first name from user
+name = gets.chomp
+#get the country
+puts "Country of origin:"
+country = gets.chomp
+#while not empty, repeat
+while !name.empty? do
+  #adds the student hash to the array
+  students << {name: name, cohort: :november, country: country, major: :ruby}
+  puts "Now we have #{students.count} students!"
+  #get another student
+ puts "Add another student name or return to quit:"
+  name = gets.chomp
+  if !name.empty?
+  puts "Country:"
+  country = gets.chomp
+else
+  puts "So I guess we are done"
+end
+end
+students
+end
+
+
+def print_header
+puts "The students of Villains Academy"
+puts "-------------"
+end
+
+def print_all(students)
+  i=0
+until i == students.length
+  puts "#{students[i][:name]} (#{students[i][:cohort]} cohort) - from #{students[i][:country]}, majoring in #{students[i][:major]}"
+  i += 1
+ end
+end
+
+
+def print_footer(students)
+puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+print_all(students)
+print_footer(students)
