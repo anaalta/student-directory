4d1b838407807e5d268c07cfb48fe709df62d26c
anaalta <suciuanamaria@yahoo.com>
2017-04-01 11:12:39 +0200

    added an if to check for typos in name

--- directory_8.7.rb
+++ directory_8.7.rb
@@ -4,36 +4,43 @@ def input_students
 #an empty array
   students = []
 #get the first name from user
-name = gets.chomp
+  name = gets.chomp.capitalize.to_sym
 #get the country
-puts "Country of origin:"
-country = gets.chomp
-puts "Now get the cohort"
-cohort = gets.chomp
+  puts "Country of origin:"
+  country = gets.chomp.capitalize.to_sym
+  puts "Now get the cohort"
+  cohort = gets.chomp.capitalize.to_sym
 #while not empty, repeat
-while !name.empty? do
-  if cohort.empty?
+  while !name.empty? do
+    if cohort.empty?
   #adds the student hash to the array
-  students << {name: name, cohort: :november , country: country, major: :ruby}
-else
-students << {name: name, cohort: cohort , country: country, major: :ruby}
-end
-  puts "Now we have #{students.count} students!"
+      students << {name: name, cohort: :november , country: country, major: :ruby}
+    else
+      students << {name: name, cohort: cohort , country: country, major: :ruby}
+    end
+    puts "Are you sure these infos are true? Check for typos in the name.Type 'y' for ok and 'n' if you want to modify"
+    typ = gets.chomp.to_s
+      if typ.downcase == "n"
+        puts "Introduce the correct name:"
+        name = gets.chomp.capitalize.to_sym
+      else
+        puts "Now we have #{students.count} students!"
+      end
   #get another student
- puts "Add another student name or return to quit:"
-  name = gets.chomp
-  if !name.empty?
-  puts "Country:"
-  country = gets.chomp
-  puts "Cohort:"
-  cohort = gets.chomp
-else
-  puts "So I guess we are done"
-end
-end
+    puts "Add another student name or return to quit:"
+    name = gets.chomp
+    if !name.empty?
+      puts "Country:"
+      country = gets.chomp.capitalize.to_sym
+      puts "Cohort:"
+      cohort = gets.chomp.capitalize.to_sym
+    else
+      puts "So I guess we are done"
+    end
+  end
+
 students
 end
-
 
 def print_header
 puts "The students of Villains Academy".center(60, '_')
@@ -41,7 +48,7 @@ end
 
 def print_all(students)
   i=0
-until i == students.length
+  until i == students.length
   puts "#{students[i][:name]} (#{students[i][:cohort]} cohort) - from #{students[i][:country]}, majoring in #{students[i][:major]}".center(80)
   i += 1
  end
