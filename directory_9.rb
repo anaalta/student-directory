c265498f043253df9f400ea974d8dbe22d2079e9
anaalta <suciuanamaria@yahoo.com>
2017-04-01 20:31:13 +0200

    refactored - with interactive menu and methods for each step

--- directory_9.rb
+++ directory_9.rb
@@ -1,33 +1,42 @@
+@students = []
 def interactive_menu
-  students = []
-loop do
+  loop do
   #1.prints the options
+  print_menu
+  #2.does what user asks
+  process(gets.chomp)
+  end
+end
+
+def print_menu
   puts "1.Input the students"
   puts "2.Show the students"
   puts "9. Exit"
-#2.put the user input into a var
-selection = gets.chomp
-#do what the user asked
-case selection
-when '1'
-  students = input_students
-when '2'
+end
+
+def process(selection)
+  #do what the user asked
+  case selection
+  when '1'
+    input_students
+  when '2'
+    show_students
+  when '9'
+    exit
+  else
+    puts "I don't know what you mean. Try again"
+  end
+end
+
+def show_students
   print_header
-  print_by_cohort(students)
-  print_footer(students)
-when '9'
-  exit
-else
-  puts "I don't know what you mean. Try again"
-end
-end
+  print_students_by_cohort(@students)
+  print_footer(@students)
 end
 
 def input_students
   puts "Please enter the name of the student:"
   puts "To finish, just skip entering the requested info"
-#an empty array
-  students = []
 #get the first name from user
   name = gets.chomp.capitalize.to_sym
 #get the country
@@ -39,9 +48,9 @@ def input_students
   while !name.empty? do
     if cohort.empty?
   #adds the student hash to the array
-      students << {name: name, cohort: :unknown , country: country, major: :ruby}
+      @students << {name: name, cohort: :unknown , country: country, major: :ruby}
     else
-      students << {name: name, cohort: cohort , country: country, major: :ruby}
+      @students << {name: name, cohort: cohort , country: country, major: :ruby}
     end
     #puts "Are you sure these infos are correct? Check for typos in the name.Type 'n' if you want to modify"
     #typ = gets.chomp.to_s
@@ -49,12 +58,11 @@ def input_students
     #    puts "Introduce the correct name:"
     #    name = gets.chomp.capitalize.to_sym
       #else
-        if students.count == 1
-        puts "Now we have #{students.count} students".slice(0..-2)
+      if @students.count == 1
+        puts "Now we have #{@students.count} students".slice(0..-2)
       else
-        puts "Now we have #{students.count} students"
+        puts "Now we have #{@students.count} students"
       end
-  #end
   #get another student
     puts "Add another student name or return to quit:"
     name = gets.chomp.capitalize.to_sym
@@ -63,11 +71,9 @@ def input_students
       country = gets.chomp.capitalize.to_sym
       puts "Cohort:"
       cohort = gets.chomp.capitalize.to_sym
-    else
-      puts "So I guess we are done"
-    end
   end
-students
+end
+@students
 end
 
 
@@ -75,18 +81,18 @@ def print_header
 puts "The students of Villains Academy".center(60, '_')
 end
 
-def print_by_cohort(students)
-  cohorts = students.group_by {|x| x[:cohort]}
+def print_students_by_cohort(students)
+  cohorts = @students.group_by {|x| x[:cohort]}
     cohorts.map do |key, value|
     puts "#{key} cohort".center(50)
       for i in 0..value.length-1
         puts "#{value[i][:name]} - #{value[i][:country]}, majoring in #{value[i][:major]}"
-  end
- end
+      end
+    end
 end
 
 def print_footer(students)
-puts "Overall, we have #{students.count} great students".center(60, '_')
+puts "Overall, we have #{@students.count} great students".center(60, '_')
 end
 
 interactive_menu
